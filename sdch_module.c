5616c334f91c3452cd32ca8c94e6ab29d8aa2e84
Dmitrij Tejblum <tejblum@yandex-team.ru>
2014-10-08 16:46:51 +0400

    NGX_HTTP_SIF_CONF does not actually work

--- sdch_module.c
+++ sdch_module.c
@@ -170,7 +170,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
@@ -186,7 +186,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_dict"),
    	  NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_TAKE1,
 	  ngx_conf_set_str_array_slot,
 	  NGX_HTTP_LOC_CONF_OFFSET,
@@ -195,7 +195,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_url"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_TAKE1,
       ngx_conf_set_str_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
@@ -204,7 +204,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_dumpdir"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_TAKE1,
       ngx_conf_set_str_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
@@ -213,7 +213,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_proxied"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_1MORE,
       ngx_conf_set_bitmask_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
@@ -223,7 +223,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_types"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_1MORE,
       ngx_http_types_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
@@ -232,7 +232,7 @@ static ngx_command_t  tr_filter_commands[] = {
 
     { ngx_string("sdch_quasi"),
       NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF
-                        |NGX_HTTP_SIF_CONF|NGX_HTTP_LIF_CONF
+                        |NGX_HTTP_LIF_CONF
                         |NGX_CONF_FLAG,
       ngx_conf_set_flag_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
